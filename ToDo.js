5469259a2edd571b2e5d2e77e50363323f3575eb
JC <jcfg67@outlook.com>
2021-03-18 08:38:50 +0100

    First commit

---
+++ ToDo.js
@@ -0,0 +1,62 @@
+const command = process.argv[2];
+const argument = process.argv[3];
+
+createFile();
+
+switch(command){
+	case '-c':
+		create(argument);
+		break;
+	case '-r':
+		readOneTask(argument);
+		break;
+	case '-u':
+		update(argument);
+		break;
+    case '-d':
+        deleteTask(argument);
+        break;
+	case 'help':
+		help();
+		break;
+	case undefined:
+		readAllTasks();
+		break;
+	default:
+		console.log(`Command not found!! 'node ToDo help' to list available commands`);
+		help();
+		break;
+}
+
+function help() {
+    console.log(`
+Use: node ToDo [-c|-r|-u|-d] [dataTask]
+"dataTask" must be a string separated with commas: "title,description,state,user"
+Use: node ToDo to list all tasks`)
+}
+
+
+function createFile() {
+    console.log(`If file 'tasks.json' doesn't exist it creates an empty one`);
+}
+
+function create(task) {
+    console.log(`Creating task ${task}`);
+}
+
+function readOneTask(task) {
+    console.log(`Reading task ${task}`);
+}
+
+function update(task) {
+    console.log(`Updating task ${task}`);
+}
+
+function deleteTask(task) {
+    console.log(`Deleting task ${task}`);
+}
+
+function readAllTasks() {
+    console.log(`Reading all tasks!!`);
+}
+
