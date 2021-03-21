564cac8b48ede1b6fea1b3a9f73db9bd44c6dfd7
Sera <sera.martin@gmail.com>
2021-03-21 21:00:59 +0100

    deleteTask v0.1

---
+++ deleteTask.js
@@ -0,0 +1,28 @@
+const Task = require('./task');
+const getTasks = require('./model').getTasks;
+const setTask = require('./model').saveTasks;
+
+var tasks = [];
+
+let obj = getTasks();
+
+
+
+function deleteTask(searched){
+
+	function checkId(element){
+		return element.id==searched;
+	}
+	
+	if(obj.findIndex(checkId)!=-1){
+		obj.splice(obj.findIndex(checkId),1);
+		setTask(obj);
+		console.log(`La tarea con ID: ${searched} ha sido eliminada.`);
+	}else{
+		console.log(`AVISO: No se encuentra la tarea con ID: ${searched}\nNO se ha eliminado nada` );
+	}
+}
+
+module.exports = deleteTask;
+
+
--- model.js
+++ model.js
@@ -18,4 +18,5 @@ function getTasks(){
     return JSON.parse(data);
 }
 
+
 module.exports = {saveTasks, getTasks}
